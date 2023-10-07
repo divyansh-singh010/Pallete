d42bb7245452f9083ef04e542e121413aa833874
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-10-08 01:08:37 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,46 +4,44 @@ import logo from './logo.png';
 import './navbar.css';
 
 const Navbar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
-    <div className={`navbar ${isSidebarOpen ? 'open' : ''}`}>
-      <div className="nav-header">
-        <Link to="/">
-          <img className="nav-logo" src={logo} alt="logo" />
-        </Link>
-        <span className="toggle-button" onClick={toggleSidebar}>
-          &#9776;
-        </span>
-      </div>
-      <nav className="nav-links">
-        <ul className="navbar-nav">
-          <li className="nav-item acad-nav-item">
-            <Link to="/" onClick={toggleSidebar}>
+    <div className={`navbar ${isDropdownOpen ? 'open' : ''}`}>
+      <Link to='/'>
+        <img className='nav-logo' src={logo} alt='logo' />
+      </Link>
+      <span className='toggle-button' onClick={toggleDropdown}>
+        &#9776;
+      </span>
+      <nav className={`nav-links ${isDropdownOpen ? 'open' : ''}`}>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <Link to='/' onClick={toggleDropdown}>
               Home
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/course" onClick={toggleSidebar}>
+          <li className='nav-item'>
+            <Link to='/course' onClick={toggleDropdown}>
               Course
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/about" onClick={toggleSidebar}>
+          <li className='nav-item'>
+            <Link to='/about' onClick={toggleDropdown}>
               Campus
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/blogs" onClick={toggleSidebar}>
+          <li className='nav-item'>
+            <Link to='/blogs' onClick={toggleDropdown}>
               Blogs
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/contact" onClick={toggleSidebar}>
+          <li className='nav-item'>
+            <Link to='/contact' onClick={toggleDropdown}>
               Contact
             </Link>
           </li>

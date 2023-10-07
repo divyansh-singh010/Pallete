c183dbfdf1d4e3bab75a56c44ac160822743d804
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-10-07 23:45:54 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -11,48 +11,36 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar">
-      <Link to="/">
-        <img className="nav-logo" src={logo} alt="logo" />
+    <div className='navbar'>
+      <Link to='/'>
+        <img className='nav-logo' src={logo} alt='logo' />
       </Link>
-      <span className="toggle-button" onClick={toggleDropdown}>
+      <span className='toggle-button' onClick={toggleDropdown}>
         &#9776;
       </span>
-      {isDropdownOpen && (
-        <div className="nav-links">
-          <nav>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/">
-                  <b>Home</b>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/course">
-                  <b>Course</b>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/about">
-                  <b>Campus</b>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/blogs">
-                  <b>Blogs</b>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contact">
-                  <b>Contact</b>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      )}
+      <div className={`nav-links ${isDropdownOpen ? 'open' : ''}`}>
+        <nav>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link to='/'><b>Home</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/course'><b>Course</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/about'><b>Campus</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/blogs'><b>Blogs</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact'><b>Contact</b></Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
-};
+}
 
 export default Navbar;

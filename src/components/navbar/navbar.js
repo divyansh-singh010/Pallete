f28c49d025004942d2e8a6d2b31437793fb1a9ad
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-10-08 01:04:19 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -13,37 +13,37 @@ const Navbar = () => {
   return (
     <div className={`navbar ${isSidebarOpen ? 'open' : ''}`}>
       <div className="nav-header">
-        <Link to='/'>
-          <img className='nav-logo' src={logo} alt='logo' />
+        <Link to="/">
+          <img className="nav-logo" src={logo} alt="logo" />
         </Link>
-        <span className='toggle-button' onClick={toggleSidebar}>
+        <span className="toggle-button" onClick={toggleSidebar}>
           &#9776;
         </span>
       </div>
       <nav className="nav-links">
-        <ul className='navbar-nav'>
-          <li className='nav-item acad-nav-item'>
-            <Link to='/' onClick={toggleSidebar}>
+        <ul className="navbar-nav">
+          <li className="nav-item acad-nav-item">
+            <Link to="/" onClick={toggleSidebar}>
               Home
             </Link>
           </li>
-          <li className='nav-item acad-nav-item'>
-            <Link to='/course' onClick={toggleSidebar}>
+          <li className="nav-item acad-nav-item">
+            <Link to="/course" onClick={toggleSidebar}>
               Course
             </Link>
           </li>
-          <li className='nav-item acad-nav-item'>
-            <Link to='/about' onClick={toggleSidebar}>
+          <li className="nav-item acad-nav-item">
+            <Link to="/about" onClick={toggleSidebar}>
               Campus
             </Link>
           </li>
-          <li className='nav-item acad-nav-item'>
-            <Link to='/blogs' onClick={toggleSidebar}>
+          <li className="nav-item acad-nav-item">
+            <Link to="/blogs" onClick={toggleSidebar}>
               Blogs
             </Link>
           </li>
-          <li className='nav-item acad-nav-item'>
-            <Link to='/contact' onClick={toggleSidebar}>
+          <li className="nav-item acad-nav-item">
+            <Link to="/contact" onClick={toggleSidebar}>
               Contact
             </Link>
           </li>

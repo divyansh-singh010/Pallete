4ff44c6d77214be97320b42c514c5fbff2f6b027
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-10-07 23:35:29 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,43 +3,44 @@ import { Link } from "react-router-dom";
 import logo from "./logo.png";
 import "./navbar.css";
 
-
 const Navbar = () => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-    return (
-        <div className='navbar'>
-            <Link to='/'><img className='nav-logo' src={logo} alt='logo' /></Link>
-            <div className={`nav-links ${isDropdownOpen ? 'open' : ''}`}>
-                <span className='toggle-button' onClick={toggleDropdown}>
-                    &#9776;
-                </span>
-                    <nav>
-                <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
-                        <li className='nav-item acad-nav-item'>
-                            <Link to='/'><b>Home</b></Link>
-                        </li>
-                        <li className='nav-item acad-nav-item'>
-                            <Link to='/course'><b>Course</b></Link>
-                        </li>
-                        <li className='nav-item acad-nav-item'>
-                            <Link to='/about'><b>Campus</b></Link>
-                        </li>
-                        <li className='nav-item acad-nav-item'>
-                            <Link to='/blogs'><b>Blogs</b></Link>
-                        </li>
-                        <li className='nav-item acad-nav-item'>
-                            <Link to='/contact'><b>Contact</b></Link>
-                        </li>
-                </ul>
-                    </nav>
-            </div>
-        </div>
-    );
+  return (
+    <div className='navbar'>
+      <Link to='/'>
+        <img className='nav-logo' src={logo} alt='logo' />
+      </Link>
+      <span className='toggle-button' onClick={toggleDropdown}>
+        &#9776;
+      </span>
+      <div className={`nav-links ${isDropdownOpen ? 'open' : ''}`}>
+        <nav>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link to='/'><b>Home</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/course'><b>Course</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/about'><b>Campus</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/blogs'><b>Blogs</b></Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact'><b>Contact</b></Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;

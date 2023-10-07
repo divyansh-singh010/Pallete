5354ea35b40599cd95bdf4005662b78b83671fde
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-10-08 01:00:31 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,94 +1,54 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import './navbar.css';
-=======
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import logo from "./logo.png";
-import "./navbar.css";
->>>>>>> 747eb5002ccace0a2afd271f13b2a3f6ac8080ce
 
 const Navbar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleDropdown = () => {
-<<<<<<< HEAD
-    setIsDropdownOpen(prev => !prev);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
-    <div className='navbar'>
-      <Link to='/'><img className='nav-logo' src={logo} alt='logo' /></Link>
-      <div className="nav-links">
-        <span className='toggle-button' onClick={toggleDropdown}>
+    <div className={`navbar ${isSidebarOpen ? 'open' : ''}`}>
+      <div className="nav-header">
+        <Link to='/'>
+          <img className='nav-logo' src={logo} alt='logo' />
+        </Link>
+        <span className='toggle-button' onClick={toggleSidebar}>
           &#9776;
         </span>
-        {isDropdownOpen && (
-          <nav>
-            <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
-              <li className='nav-item acad-nav-item'>
-                <Link to='/'>Home</Link>
-              </li>
-              <li className='nav-item acad-nav-item'>
-                <Link to='/course'>Course</Link>
-              </li>
-              <li className='nav-item acad-nav-item'>
-                <Link to='/about'>Campus</Link>
-              </li>
-              <li className='nav-item acad-nav-item'>
-                <Link to='/blogs'>Blogs</Link>
-              </li>
-              <li className='nav-item acad-nav-item'>
-                <Link to='/contact'>Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        )}
       </div>
-=======
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  return (
-    <div className="navbar">
-      <Link to="/">
-        <img className="nav-logo" src={logo} alt="logo" />
-      </Link>
-      <span className="toggle-button" onClick={toggleDropdown}>
-        &#9776;
-      </span>
-      <nav className={`nav-links ${isDropdownOpen ? 'open' : ''}`}>
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item acad-nav-item">
-            <Link to="/">
-              <b>Home</b>
+      <nav className="nav-links">
+        <ul className='navbar-nav'>
+          <li className='nav-item acad-nav-item'>
+            <Link to='/' onClick={toggleSidebar}>
+              Home
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/course">
-              <b>Course</b>
+          <li className='nav-item acad-nav-item'>
+            <Link to='/course' onClick={toggleSidebar}>
+              Course
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/about">
-              <b>Campus</b>
+          <li className='nav-item acad-nav-item'>
+            <Link to='/about' onClick={toggleSidebar}>
+              Campus
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/blogs">
-              <b>Blogs</b>
+          <li className='nav-item acad-nav-item'>
+            <Link to='/blogs' onClick={toggleSidebar}>
+              Blogs
             </Link>
           </li>
-          <li className="nav-item acad-nav-item">
-            <Link to="/contact">
-              <b>Contact</b>
+          <li className='nav-item acad-nav-item'>
+            <Link to='/contact' onClick={toggleSidebar}>
+              Contact
             </Link>
           </li>
         </ul>
       </nav>
->>>>>>> 747eb5002ccace0a2afd271f13b2a3f6ac8080ce
     </div>
   );
 };

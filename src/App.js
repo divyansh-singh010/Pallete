549512f1647d6a30adab4d237ef5c86b1105ffea
Sarthak <122222018+Sarthakagg31@users.noreply.github.com>
2023-10-09 03:06:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,29 +28,36 @@ const App = () => {
         </Routes>
         {/* footer starts here */}
         <div className='footer'>
-          <div className='footer-logo'>
-            <img src={logo} alt='logo' />
-          </div>
-          <div className='footer-contact'>
-            <h4 className='footer-text'>New Delhi, Delhi</h4>
-            <h4 className='footer-text2'>
-              Phone: +91 98202 67890<br />
-              Email: xyz1223444531@gmail.com
-            </h4>
-          </div>
-          <div className='footer-links'>
-            <table className='tables">
-            <tr><h6 className='footer-text4'>FAQ</h6></tr>
-            <tr><h6 className='footer-text4'>Events</h6></tr>
-             <tr><h6 className='footer-text4'>Privacy Policy</h6></tr>
-            </table>
-           </div>
-           <h6 className='footer-text3'>
-             © 2023 Palate Culinary Academy. All Rights Reserved.
-           </h6>
-         </div>
-         {/* footer ends here */}
-      </div>
+  <div className='footer-logo'>
+    <img src={logo} alt='logo' />
+  </div>
+  <div className='footer-contact'>
+    <h4 className='footer-text'>New Delhi, Delhi</h4>
+    <h4 className='footer-text2'>
+      Phone: +91 98202 67890<br />
+      Email: xyz1223444531@gmail.com
+    </h4>
+  </div>
+  <div className='footer-links'>
+    <table className='tables'>
+      <tbody>
+        <tr>
+          <td><h6 className='footer-text4'>FAQ</h6></td>
+        </tr>
+        <tr>
+          <td><h6 className='footer-text4'>Events</h6></td>
+        </tr>
+        <tr>
+          <td><h6 className='footer-text4'>Privacy Policy</h6></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <h6 className='footer-text3'>
+    © 2023 Palate Culinary Academy. All Rights Reserved.
+  </h6>
+</div>
+
     </Router>
   );
 }

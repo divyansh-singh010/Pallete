1c021aedba5c11ee594c339da17eca168a9e6aec
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2023-10-08 16:32:10 +0530

    final-changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,46 +7,42 @@ const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+    setIsDropdownOpen(prev => !prev);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
   };
 
   return (
-    <div className={`navbar ${isDropdownOpen ? 'open' : ''}`}>
-      <Link to='/'>
-        <img className='nav-logo' src={logo} alt='logo' />
-      </Link>
-      <span className='toggle-button' onClick={toggleDropdown}>
-        &#9776;
-      </span>
-      <nav className={`nav-links ${isDropdownOpen ? 'open' : ''}`}>
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <Link to='/' onClick={toggleDropdown}>
-              Home
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/course' onClick={toggleDropdown}>
-              Course
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/about' onClick={toggleDropdown}>
-              Campus
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/blogs' onClick={toggleDropdown}>
-              Blogs
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/contact' onClick={toggleDropdown}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className='navbar'>
+      <Link to='/'><img className='nav-logo' src={logo} alt='logo' /></Link>
+      <div className="nav-links">
+        <span className='toggle-button' onClick={toggleDropdown}>
+          &#9776;
+        </span>
+        {isDropdownOpen && (
+          <nav onBlur={closeDropdown}>
+            <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+              <li className='nav-item acad-nav-item'>
+                <Link to='/' onClick={closeDropdown}>Home</Link>
+              </li>
+              <li className='nav-item acad-nav-item'>
+                <Link to='/course' onClick={closeDropdown}>Course</Link>
+              </li>
+              <li className='nav-item acad-nav-item'>
+                <Link to='/about' onClick={closeDropdown}>Campus</Link>
+              </li>
+              <li className='nav-item acad-nav-item'>
+                <Link to='/blogs' onClick={closeDropdown}>Blogs</Link>
+              </li>
+              <li className='nav-item acad-nav-item'>
+                <Link to='/contact' onClick={closeDropdown}>Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
     </div>
   );
 };

1b0d568fd890279f2089c76c5d9fb65f71ace58d
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2023-10-08 22:09:07 +0530

    images_set

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,28 +6,25 @@ import img1 from "./img1.png";
 import img2 from "./img2.png";
 import img3 from "./img3.png";
 import logo from "./logo.png";
+import image1 from "./1.png";
+import rakhee from "./2.png";
 
 const Home = () => {
   return (<>
-
-
-
-
-
     <div className="home">
       <div className="home-bg-text">
-      <p className="home-text">
+        <p className="home-text">
           palate
           <br />
           culinary
           <br />
           academy
-          </p>
-          </div>
-          <div className="home-bg">
-          </div>
-          <div className="home">
-          <div className="home-images">
+        </p>
+      </div>
+      <div className="home-bg">
+      </div>
+      <div className="home">
+        <div className="home-images">
           <div className="img-item">
             <img className="home-img mid-img" src={img1} alt="profile" />
           </div>
@@ -35,49 +32,49 @@ const Home = () => {
             <img className="home-img mid-img" src={img2} alt="profile" />
           </div>
           <div className="img-item">
-          <img className="home-img mid-img" src={img3} alt="profile" />
+            <img className="home-img mid-img" src={img3} alt="profile" />
           </div>
-          </div>
-          <div className="container4">
+        </div>
+        <div className="container4">
           <div className="image1">
-          <img className="home-text-img" src={logo} alt="profile" />
+            <img className="home-text-img" src={image1} alt="profile" />
           </div>
           <div className="welcome-text">
-          <p className="home-welcome-text">
-          WELCOME To palate Culinary Academy
-          </p>
-          <p className="home-welcome-text2">
-          Palate Culinary Academy is an International culinary and pastry
-          arts education, driven by the vision of Celebrity chef Rakhee
-          Vaswani, Our outstanding courses are tailored for your goals,
-          offering hands-on training for Bachelor students, professionals,
-          career-changers, and entrepreneurs. Led by experienced chefs and
-          industry experts, you’ll take your skills to a world-class standard
-          driven by the vision of Celebrity chef Rakhee Vaswani,
+            <p className="home-welcome-text">
+              WELCOME To palate Culinary Academy
+            </p>
+            <p className="home-welcome-text2">
+              Palate Culinary Academy is an International culinary and pastry
+              arts education, driven by the vision of Celebrity chef Rakhee
+              Vaswani, Our outstanding courses are tailored for your goals,
+              offering hands-on training for Bachelor students, professionals,
+              career-changers, and entrepreneurs. Led by experienced chefs and
+              industry experts, you’ll take your skills to a world-class standard
+              driven by the vision of Celebrity chef Rakhee Vaswani,
             </p>
           </div>
-          </div>
-          </div>
-          </div>
-          <div className="rakhee">
-            <div className="rakhee-bg">
-              <img className="rakhee-bg-img" src={homebg} alt="profile" />
-              </div>
-            <div className="rakhee-head">
-              Rakhee Vaswani
-            </div>
-            <div className="rakhee-subhead">
-              Head Chef & Founder
-            </div>
-            <hr className="rakhee-rule" />
-            <div className="rakhee-text">
-            Sed pellentesque venenatis tincidunt. Maecenas at mattis quam, ut bibendum odio. Nunc vitae tempor velit, in pharetra ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque molestie tortor nec erat vestibulum condimentum. Nam dui ante, tempus a sollicitudin id, vestibulum eget lectus. Maecenas malesuada quam sit amet mauris dapibus 
-              </div>
-          </div>
-          
-          <div className="container3">
-          <h3 className="courses">Courses We Offer</h3>
-          <hr className="rule" />
+        </div>
+      </div>
+    </div>
+    <div className="rakhee">
+      <div className="rakhee-bg">
+        <img className="rakhee-bg-img" src={rakhee} alt="profile" />
+      </div>
+      <div className="rakhee-head">
+        Rakhee Vaswani
+      </div>
+      <div className="rakhee-subhead">
+        Head Chef & Founder
+      </div>
+      <hr className="rakhee-rule" />
+      <div className="rakhee-text">
+        Sed pellentesque venenatis tincidunt. Maecenas at mattis quam, ut bibendum odio. Nunc vitae tempor velit, in pharetra ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque molestie tortor nec erat vestibulum condimentum. Nam dui ante, tempus a sollicitudin id, vestibulum eget lectus. Maecenas malesuada quam sit amet mauris dapibus
+      </div>
+    </div>
+
+    <div className="container3">
+      <h3 className="courses">Courses We Offer</h3>
+      <hr className="rule" />
       <p className="courses-text">
         We offer not only UK Certified Culinary, Patisserie & Bakery courses,
         but also over 150 one day workshops, many other international
